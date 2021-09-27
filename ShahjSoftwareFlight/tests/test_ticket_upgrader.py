9a1da3c0ba8744d83f47234f81eedf2f371d7ce8
Gokhale <Poornima.Gokhale@emc.com>
2021-09-28 00:28:52 +0530

    added unit tests

---
+++ ShahjSoftwareFlight/tests/test_ticket_upgrader.py
@@ -0,0 +1,57 @@
+import unittest
+from flight_ticket_upgrader.constants.regex import *
+
+
+class TestTicketUpgrader(unittest.TestCase):
+    def test_mobile_validator(self):
+        """
+        Tests that given data is a valid mobile number
+        :return:
+        """
+
+        self.assertRegexpMatches("9876543219", mobile_regex)
+        self.assertRegexpMatches("8886543219", mobile_regex)
+        self.assertRegexpMatches("6543219000", mobile_regex)
+
+        self.assertNotRegexpMatches("98765432190", mobile_regex)
+        self.assertNotRegexpMatches("98765432", mobile_regex)
+        self.assertNotRegexpMatches("98765432$%", mobile_regex)
+        self.assertNotRegexpMatches("!@#$%98765", mobile_regex)
+
+    def test_email_validator(self):
+        """
+        Tests that given data is a valid email id
+        :return:
+        """
+
+        self.assertRegexpMatches("abc.xyz@ppp.com", email_regex)
+        self.assertRegexpMatches("abc+xyz@sss.co", email_regex)
+        self.assertRegexpMatches("abc-xyz@f.co.in", email_regex)
+        self.assertRegexpMatches("abcdx_yz@sd-gf.com", email_regex)
+
+        self.assertNotRegexpMatches("@ggg.com", email_regex)
+        self.assertNotRegexpMatches("abcd@", email_regex)
+        self.assertNotRegexpMatches("abcd@abcd", email_regex)
+        self.assertNotRegexpMatches("123456", email_regex)
+        self.assertNotRegexpMatches("", email_regex)
+
+    def test_pnr_validator(self):
+        """
+        Tests that given data is a valid pnr
+        :return:
+        """
+
+        self.assertRegexpMatches("AB1234", pnr_regex)
+        self.assertRegexpMatches("pqrs12", pnr_regex)
+        self.assertRegexpMatches("STHYYY", pnr_regex)
+        self.assertRegexpMatches("123456", pnr_regex)
+
+        self.assertNotRegexpMatches("WXE1234", email_regex)
+        self.assertNotRegexpMatches("WXE12", email_regex)
+        self.assertNotRegexpMatches("ABC#12", email_regex)
+        self.assertNotRegexpMatches("ABC#$%", email_regex)
+        self.assertNotRegexpMatches(":?!@#$", email_regex)
+
+
+if __name__ == '__main__':
+    unittest.main()
